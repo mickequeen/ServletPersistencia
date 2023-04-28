6e2f032eaa84bbb93cd79da46bef34f20be28f71
mickequeen <mickequeen@gmail.com>
2023-04-28 00:20:13 -0400

    insert

--- src/main/java/root/model/entities/MasterCliente.java
+++ src/main/java/root/model/entities/MasterCliente.java
@@ -6,15 +6,15 @@ package root.model.entities;
 
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 /**
  *
